5d2e74f5ec4445bade6b66f87eb99745053e8018
RaymondSimons <rsimons@jhu.edu>
2018-10-18 22:36:49 -0400

    default testing message

---
+++ find_satellite_centers.py
@@ -0,0 +1,24 @@
+import astropy
+from astropy.io import fits
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print 'hi'
---
+++ track_satellites.py
@@ -0,0 +1,15 @@
+import astropy
+from astropy.io import fits
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    min_dd
+    max_dd = 
