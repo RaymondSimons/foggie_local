1fc79caeb83d84c0082aa13f71fe6392ea71881e
RaymondSimons <rsimons@jhu.edu>
2018-10-18 23:38:39 -0400

    default testing message

--- make_satellite_figure.py
+++ make_satellite_figure.py
@@ -6,7 +6,7 @@ import math
 from joblib import Parallel, delayed
 import os, sys, argparse
 import yt
-import matploblib.pyplot as plt
+import matplotlib.pyplot as plt
 def parse():
     '''
     Parse command line arguments
@@ -24,6 +24,7 @@ def parse():
 
     args = vars(parser.parse_args())
     return args
+
 
 
 
