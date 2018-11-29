1cdb5610bc6155fe2fb52c655b7ec967c1179b6f
RaymondSimons <rsimons@jhu.edu>
2018-11-28 21:13:13 -0500

    default testing message

--- track_satellites.py
+++ track_satellites.py
@@ -28,12 +28,14 @@ def parse():
 
 
 
-def weighted_avg_and_std(values, weights):
+def weighted_avg_and_std(values, weights, good):
     """
     Return the weighted average and standard deviation.
 
     values, weights -- Numpy ndarrays with the same shape.
     """
+    values = values[good]
+    weights = weights[good]
     average = np.average(values, weights=weights)
     # Fast and numerically precise:
     variance = np.average((values-average)**2, weights=weights)
@@ -62,6 +64,9 @@ def make_savefile(anchor_fits, DD, simname):
 
 
 
+    hdus = []
+    prim_hdu = fits.PrimaryHDU()
+    hdus.append(prim_hdu)
 
     for sat_n in arange(5):
 
@@ -74,13 +79,13 @@ def make_savefile(anchor_fits, DD, simname):
         for g in arange(len(anchor_ids)):
             gd_indices[g] = int(where(id_s == anchor_ids[g])[0])
 
-        anchor_mss    = mss[gd_indices]
-        anchor_xs    = xs[gd_indices]
-        anchor_ys    = ys[gd_indices]
-        anchor_zs    = zs[gd_indices]
-        anchor_vxs   = vxs[gd_indices]
-        anchor_vys   = vys[gd_indices]
-        anchor_vzs   = vzs[gd_indices]
+        anchor_mss      = mss[gd_indices]
+        anchor_xs       = xs[gd_indices]
+        anchor_ys       = ys[gd_indices]
+        anchor_zs       = zs[gd_indices]
+        anchor_vxs      = vxs[gd_indices]
+        anchor_vys      = vys[gd_indices]
+        anchor_vzs      = vzs[gd_indices]
 
         anchor_xs_box    =  xs_box[gd_indices]
         anchor_ys_box    =  ys_box[gd_indices]
@@ -89,18 +94,46 @@ def make_savefile(anchor_fits, DD, simname):
         anchor_vys_box   = vys_box[gd_indices]
         anchor_vzs_box   = vzs_box[gd_indices]
 
-        anchor_xs_avg, _ = weighted_avg_and_std(anchor_xs,  weights = anchor_mss)
-        anchor_ys_avg, _ = weighted_avg_and_std(anchor_ys,  weights = anchor_mss)
-        anchor_zs_avg, _ = weighted_avg_and_std(anchor_zs, weights = anchor_mss)
-        anchor_vxs_avg, _= weighted_avg_and_std(anchor_vxs, weights = anchor_mss)
-        anchor_vys_avg, _= weighted_avg_and_std(anchor_vys, weights = anchor_mss)
-        anchor_vzs_avg, _= weighted_avg_and_std(anchor_vzs, weights = anchor_mss)
-        anchor_xs_box_avg, _  = weighted_avg_and_std(anchor_xs_box, weights = anchor_mss)
-        anchor_ys_box_avg, _  = weighted_avg_and_std(anchor_ys_box, weights = anchor_mss)
-        anchor_zs_box_avg, _  = weighted_avg_and_std(anchor_zs_box, weights = anchor_mss)
-        anchor_vxs_box_avg, _ = weighted_avg_and_std(anchor_vxs_box, weights = anchor_mss)
-        anchor_vys_box_avg, _ = weighted_avg_and_std(anchor_vys_box, weights = anchor_mss)
-        anchor_vzs_box_avg, _ = weighted_avg_and_std(anchor_vzs_box, weights = anchor_mss)
+
+        good = where((abs(anchor_xs - median(anchor_xs)) < 5) & (abs(anchor_ys - median(anchor_ys)) < 5) & (abs(anchor_zs - median(anchor_zs)) < 5))          
+
+
+
+
+
+        anchor_xs_avg, _ = weighted_avg_and_std(anchor_xs,  weights = anchor_mss, good = good)
+        anchor_ys_avg, _ = weighted_avg_and_std(anchor_ys,  weights = anchor_mss, good = good)
+        anchor_zs_avg, _ = weighted_avg_and_std(anchor_zs,  weights = anchor_mss, good = good)
+        anchor_vxs_avg, _= weighted_avg_and_std(anchor_vxs, weights = anchor_mss, good = good)
+        anchor_vys_avg, _= weighted_avg_and_std(anchor_vys, weights = anchor_mss, good = good)
+        anchor_vzs_avg, _= weighted_avg_and_std(anchor_vzs, weights = anchor_mss, good = good)
+        anchor_xs_box_avg, _  = weighted_avg_and_std(anchor_xs_box,  weights = anchor_mss, good = good)
+        anchor_ys_box_avg, _  = weighted_avg_and_std(anchor_ys_box,  weights = anchor_mss, good = good)
+        anchor_zs_box_avg, _  = weighted_avg_and_std(anchor_zs_box,  weights = anchor_mss, good = good)
+        anchor_vxs_box_avg, _ = weighted_avg_and_std(anchor_vxs_box, weights = anchor_mss, good = good)
+        anchor_vys_box_avg, _ = weighted_avg_and_std(anchor_vys_box, weights = anchor_mss, good = good)
+        anchor_vzs_box_avg, _ = weighted_avg_and_std(anchor_vzs_box, weights = anchor_mss, good = good)
+
+        cols1 = fits.Column(name = 'anchor_mss     ', array =  anchor_mss    , format = 'D')
+         fits.Column(name = 'anchor_xs      ', array =  anchor_xs     , format = 'D')
+         fits.Column(name = 'anchor_ys      ', array =  anchor_ys     , format = 'D')
+         fits.Column(name = 'anchor_zs      ', array =  anchor_zs     , format = 'D')
+         fits.Column(name = 'anchor_vxs     ', array =  anchor_vxs    , format = 'D')
+         fits.Column(name = 'anchor_vys     ', array =  anchor_vys    , format = 'D')
+         fits.Column(name = 'anchor_vzs     ', array =  anchor_vzs    , format = 'D')
+         fits.Column(name = 'anchor_xs_box  ', array =  anchor_xs_box , format = 'D')
+         fits.Column(name = 'anchor_ys_box  ', array =  anchor_ys_box , format = 'D')
+         fits.Column(name = 'anchor_zs_box  ', array =  anchor_zs_box , format = 'D')
+         fits.Column(name = 'anchor_vxs_box ', array =  anchor_vxs_box, format = 'D')
+         fits.Column(name = 'anchor_vys_box ', array =  anchor_vys_box, format = 'D')
+         fits.Column(name = 'anchor_vzs_box ', array =  anchor_vzs_box, format = 'D')
+
+
+
+        hdus.append(fits.BinTableHDU.from_columns(cols1, name = 'table_%.2i'%sat_n))
+
+
+
 
 
         to_save = [anchor_xs_box_avg, anchor_ys_box_avg, anchor_zs_box_avg, anchor_vxs_box_avg, anchor_vys_box_avg, anchor_vzs_box_avg, anchor_xs_avg, anchor_ys_avg, anchor_zs_avg, anchor_vxs_avg, anchor_vys_avg, anchor_vzs_avg]
@@ -109,6 +142,10 @@ def make_savefile(anchor_fits, DD, simname):
 
         print '/nobackupp2/rcsimons/foggie_momentum/anchor_files/%s_DD%.4i_sat%.2i_cen.npy'%(simname, DD, sat_n)
         np.save('/nobackupp2/rcsimons/foggie_momentum/anchor_files/%s_DD%.4i_sat%.2i_cen.npy'%(simname, DD, sat_n),to_save)
+
+
+    hdus_fits = fits.HDUList(hdus)
+    hdus_fits.writeto('/nobackupp2/rcsimons/foggie_momentum/anchor_files/%s_DD%.4i_sat%.2i_anchorprops.fits'%(simname, DD, sat_n), overwrite = True)
 
 
 
