a394d757dc56168dd1c66c4bc2c33360e001fa4e
RaymondSimons <rsimons@jhu.edu>
2018-11-29 16:57:51 -0500

    default testing message

--- nparticles.py
+++ nparticles.py
@@ -37,6 +37,7 @@ if __name__ == '__main__':
     DD = int(args['DD'])
     haloname = args['haloname']
 
+
     snapname = 'DD%.4i'%DD
 
     ds = yt.load('/nobackupp2/mpeeples/%s/%s/%s/%s'%(haloname, simname, snapname, snapname))
