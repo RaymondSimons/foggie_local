f114731630313223b3e42d47fde489fd2eacc961
RaymondSimons <rsimons@jhu.edu>
2018-12-03 16:49:19 -0500

    default testing message

--- make_movie_frames.py
+++ make_movie_frames.py
@@ -89,11 +89,11 @@ if __name__ == '__main__':
 
 
     if not add_cbar:
-        fig, ax = plt.subplots(1,2, figsize = (20,10))
+        fig, axes = plt.subplots(1,2, figsize = (20,10))
         fig.subplots_adjust(left = 0.0, right = 1.0, top =1.0, bottom = 0.0)
 
     else:
-        fig, ax = plt.subplots(1,1, figsize = (10,9))
+        fig, axes = plt.subplots(1,1, figsize = (10,9))
         fig.subplots_adjust(left = 0.0, right = 0.92, top =1.0, bottom = 0.0, hspace = 0.0, wspace = 0.0)
 
 
