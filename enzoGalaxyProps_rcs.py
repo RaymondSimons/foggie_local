db0dde4ef00f3697577a67385c83f94d4e4cf07f
RaymondSimons <rsimons@jhu.edu>
2019-01-28 13:14:17 -0500

    default testing message

--- enzoGalaxyProps_rcs.py
+++ enzoGalaxyProps_rcs.py
@@ -399,28 +399,6 @@ if __name__=="__main__":
     #simname = os.path.basename(dirname) #assumes directory name for simulation name
 
     print( "Simulation name:  ", simname)
-    '''
-    particle_headers = []
-    particle_data = []
-    stars_data = []
-    new_snapfiles = []
-    for sn in snaps:
-        aname=os.path.basename(sn)
-        adir=os.path.abspath(os.path.dirname(sn))
-        snap_dir = os.path.join(adir,simname+'_'+aname+'_sunrise')
-        yt_fig_dir = snap_dir+'/yt_projections'
-        print( "Sunrise directory: ", snap_dir)
-        if not os.path.lexists(snap_dir):
-            print ("Creating Sunrise directory:", snap_dir)
-            os.mkdir(snap_dir)        
-        if not os.path.lexists(yt_fig_dir):
-            print ("Creating YT figure directory:", yt_fig_dir)
-            os.mkdir(yt_fig_dir)        
-
-
-        new_snapfiles.append(os.path.abspath(sn))
-    new_snapfiles = np.asarray(new_snapfiles)
-    '''
 
     new_snapfiles = np.asarray(snaps)
 
