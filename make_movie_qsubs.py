042f8605bd63a0a122115a4ab6d85eb299aabd32
RaymondSimons <rsimons@jhu.edu>
2019-02-04 11:31:52 -0500

    default testing message

---
+++ make_movie_qsubs.py
@@ -0,0 +1,67 @@
+import numpy
+from numpy import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+for simname in ['nref11n_nref10f_selfshield_z6', 'nref11n_selfshield_z15']:
+
+    cen_fits = np.load('/Users/rsimons/Dropbox/rcs_foggie/catalogs/center_%s.npy'%simname)[()]
+    xf = cen_fits['x']
+    yf = cen_fits['y']
+    zf = cen_fits['z']
+
+    for DDmin in arange(200, 1050, 100):
+        DDmax = DDmin + 100
+        sf = open('./qsubs/submit_%s_%i_%i_movie.sh'%(simname, DDmin, DDmax), 'w+')
+
+        for DD in arange(DDmin, DDmax):
+            snap_name = 'DD%.2i'%(DD)
+            sim_snap_name = snap_name + '_' + simname
+            qsub_fname = '%s_movie.qsub'%(sim_snap_name)
+            qf = open('./qsubs/%s'%qsub_fname, 'w+')
+            
+            qf.write('#PBS -S /bin/bash\n')
+            qf.write('#PBS -l select=1:ncpus=16:model=san\n')
+            qf.write('#PBS -l walltime=00:20:00\n')
+            qf.write('#PBS -q normal\n')
+            qf.write('#PBS -N %s\n'%sim_snap_name)
+            qf.write('#PBS -M rsimons@jhu.edu\n')
+            qf.write('#PBS -m abe\n')
+            qf.write('#PBS -o ./outfiles/%s_pbs.out\n'%sim_snap_name)
+            qf.write('#PBS -e ./outfiles/%s_pbs.err\n'%sim_snap_name)
+            qf.write('#PBS -V\n')
+            qf.write('#PBS -W group_list=s1698\n\n\n\n')      
+            cenx = xf[0] * DD**4. + xf[1] * DD**3. + xf[2] * DD**2. + xf[3] * DD + xf[4]
+            ceny = yf[0] * DD**4. + yf[1] * DD**3. + yf[2] * DD**2. + yf[3] * DD + yf[4]
+            cenz = zf[0] * DD**4. + zf[1] * DD**3. + zf[2] * DD**2. + zf[3] * DD + zf[4]
+
+            Lx = 1.
+            Ly = 0.
+            Lz = 0.
+
+            out_string =' > ./outfiles/%s_%.4i_movie.err > ./outfiles/%s_%.4i_movie.out'%(simname, DD, simname, DD)
+            #qf.write('python /u/rcsimons/scripts/foggie_local/make_movie.py -DD %i -simname %s -cenx %.2f -ceny %.2f -cenz %.2f --lx %.6f --ly %.6f --lz %.6f %s\n'%(DD, simname, cenx, ceny, cenz, Lx, Ly, Lz, out_string))
+            qf.write('python /u/rcsimons/scripts/foggie_local/make_movie.py -DD %i -simname %s -cenx %.4f -ceny %.4f -cenz %.4f %s\n'%(DD, simname, cenx, ceny, cenz, out_string))
+
+            qf.close()
+
+            sf.write('qsub %s\n'%qsub_fname)
+
+
+        sf.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
