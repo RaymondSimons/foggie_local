00834f95bb05fac1e1abd2cc9ce4ed0bcc4ee92e
RaymondSimons <rsimons@jhu.edu>
2019-02-05 13:25:48 -0500

    default testing message

--- track_satellites.py
+++ track_satellites.py
@@ -161,7 +161,7 @@ if __name__ == '__main__':
     #anchor_ids = np.load('/nobackupp2/rcsimons/foggie_momentum/anchor_files/%s_anchors.npy'%simname)
 
     #anchor_fits = fits.open('/nobackupp2/rcsimons/foggie_momentum/anchor_files/%s_anchors_DD0250.fits'%simname)
-    anchor_fits = np.load('/nobackupp2/rcsimons/foggie_momentum/anchor_files/%s_DD1049_anchors.npy'%simname)[()]
+    anchor_fits = np.load('/nobackupp2/rcsimons/foggie_momentum/catalogs/%s_DD1049_anchors.npy'%simname)[()]
 
     Parallel(n_jobs = -1, backend = 'threading')(delayed(make_savefile)(anchor_fits = anchor_fits, DD = DD, simname = simname, anchor_str = '1049') for DD in np.arange(min_DD, max_DD))
 
