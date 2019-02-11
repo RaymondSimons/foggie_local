9a6e738e6faca189717fdfacb39bdbe2817c05e0
RaymondSimons <rsimons@jhu.edu>
2019-02-11 17:56:49 -0500

    default testing message

--- make_satmass_qsubfiles.py
+++ make_satmass_qsubfiles.py
@@ -21,7 +21,7 @@ for sim_name in ['nref11n_selfshield_z15', 'nref11n_nref10f_selfshield_z6']:
         
         qf.write('#PBS -S /bin/bash\n')
         qf.write('#PBS -l select=1:ncpus=16:model=san\n')
-        qf.write('#PBS -l walltime=01:00:00\n')
+        qf.write('#PBS -l walltime=03:00:00\n')
         qf.write('#PBS -q normal\n')
         qf.write('#PBS -N %s\n'%sim_snap_name)
         qf.write('#PBS -M rsimons@jhu.edu\n')
