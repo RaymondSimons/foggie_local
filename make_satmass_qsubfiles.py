ab1e0b31922c2bcb3ae0545846c5a1e28df92c8d
RaymondSimons <rsimons@jhu.edu>
2019-02-11 17:55:51 -0500

    default testing message

--- make_satmass_qsubfiles.py
+++ make_satmass_qsubfiles.py
@@ -10,7 +10,7 @@ for sim_name in ['nref11n_selfshield_z15', 'nref11n_nref10f_selfshield_z6']:
     DDmax = 1050
     N_split = 5
 
-    sf = open('/nobackupp2/rcsimons/foggie_momentum/submit_scripts/submit_%s_%i_%i_satmass_qsub.sh'%(sim_name, ddmin, ddmax), 'w+')
+    sf = open('/nobackupp2/rcsimons/foggie_momentum/submit_scripts/submit_%s_%i_%i_satmass_qsub.sh'%(sim_name, DDmin, DDmax), 'w+')
     for DD in arange(DDmin, DDmax, N_split):
         snap_name = 'DD%.4i_DD%.4i'%(DD, DD + N_split)
         sim_snap_name = snap_name + '_' + sim_name+'_satmass'
