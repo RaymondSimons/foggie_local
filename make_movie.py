483c0571c774a1728f1bd4e331ffa4143b0ab18d
RaymondSimons <rsimons@jhu.edu>
2019-04-24 16:28:14 -0400

    default testing message

--- make_movie.py
+++ make_movie.py
@@ -124,7 +124,7 @@ def make_figure(sat_n, figdir, wd, wdd, DD, cen_name, simdir, haloname, simname)
                 figname = '%s_%.4i_%.2i_%s.png'%(cen_name, DD, sat_n, axis)
                 fig.savefig('%s/%s'%(figdir,figname))
                 plt.close(fig)
-                ds.index.clear_all_data()
+            ds.index.clear_all_data()       
 if __name__ == '__main__':
 
     args = parse()
@@ -152,7 +152,7 @@ if __name__ == '__main__':
         for sat_n in arange(12):
             lst.append((DD, sat_n))
 
-    Parallel(n_jobs = 6)(delayed(make_figure)(sat_n, figdir, wd, wdd, DD, cen_name, simdir, haloname, simname) for (DD, sat_n) in lst)
+    Parallel(n_jobs = 3)(delayed(make_figure)(sat_n, figdir, wd, wdd, DD, cen_name, simdir, haloname, simname) for (DD, sat_n) in lst)
 
 
 
