e48915b0e05e1a9f6f38792b32b87d9ffdb65db4
RaymondSimons <rsimons@jhu.edu>
2019-04-24 13:10:25 -0400

    default testing message

--- make_satellite_figure.py
+++ make_satellite_figure.py
@@ -63,56 +63,57 @@ if __name__ == '__main__':
 
     #anchor_xs_box_avg, anchor_ys_box_avg, anchor_zs_box_avg, anchor_vxs_box_avg, anchor_vys_box_avg, anchor_vzs_box_avg = cen_file
     for sat_n in arange(12):
-        cenx = cen_fits['SAT_%.2i'%sat_n].data['box_avg'][0]
-        ceny = cen_fits['SAT_%.2i'%sat_n].data['box_avg'][1]
-        cenz = cen_fits['SAT_%.2i'%sat_n].data['box_avg'][2]
-        cen = yt.YTArray([cenx, ceny, cenz], 'kpc')
+        if len(cen_fits['SAT_%.2i'%sat_n].data['box_avg']) > 0:
+            cenx = cen_fits['SAT_%.2i'%sat_n].data['box_avg'][0]
+            ceny = cen_fits['SAT_%.2i'%sat_n].data['box_avg'][1]
+            cenz = cen_fits['SAT_%.2i'%sat_n].data['box_avg'][2]
+            cen = yt.YTArray([cenx, ceny, cenz], 'kpc')
 
-        #vel_vec = array([anchor_vxs_box_avg, anchor_vys_box_avg, anchor_vzs_box_avg])
-        #L = vel_vec/np.linalg.norm(vel_vec)
-
-
-        #L = [1*math.cos(pi*(DD)/100.),0, 1*math.sin(pi*(DD)/100.)] # vector normal to cutting plane
-
-        fig, axes = plt.subplots(3,2, figsize = (10.8, 15))
+            #vel_vec = array([anchor_vxs_box_avg, anchor_vys_box_avg, anchor_vzs_box_avg])
+            #L = vel_vec/np.linalg.norm(vel_vec)
 
 
-        for i in arange(3):
-            L = Ls[i]
-            image1 = yt.off_axis_projection(ds, cen, L, W1, N, ('gas', 'density'), north_vector =  north_vector)
-            image2 = yt.off_axis_projection(ds, cen, L, W2, N, ('gas', 'density'), north_vector =  north_vector)
+            #L = [1*math.cos(pi*(DD)/100.),0, 1*math.sin(pi*(DD)/100.)] # vector normal to cutting plane
 
-            image1 = image1.in_units('Msun * kpc**-2')
-            image2 = image2.in_units('Msun * kpc**-2')
-
-            im1 = axes[i, 0].imshow(np.log10(image1), vmin = 4.5, vmax = 9.5)
-            im2 = axes[i, 1].imshow(np.log10(image2), vmin = 4.5, vmax = 9.5)
+            fig, axes = plt.subplots(3,2, figsize = (10.8, 15))
 
 
-            bar_len_kpc = 1.
-            bar_len_pix = 1.*N/W1[0].value * bar_len_kpc
-            y_bar_start_pix = 0.5*N
-            y_bar_end_pix = y_bar_start_pix + bar_len_pix
-            x_bar_pix = 0.9*N
-            axes[i, 0].plot([x_bar_pix, x_bar_pix], [y_bar_start_pix, y_bar_end_pix], color = 'white', linewidth = 3)
-            axes[i, 0].annotate("%i kpc"%bar_len_kpc, (x_bar_pix-0.08*N, y_bar_start_pix- 0.04*N), color = 'white', fontsize = 15, rotation = 0)
+            for i in arange(3):
+                L = Ls[i]
+                image1 = yt.off_axis_projection(ds, cen, L, W1, N, ('gas', 'density'), north_vector =  north_vector)
+                image2 = yt.off_axis_projection(ds, cen, L, W2, N, ('gas', 'density'), north_vector =  north_vector)
+
+                image1 = image1.in_units('Msun * kpc**-2')
+                image2 = image2.in_units('Msun * kpc**-2')
+
+                im1 = axes[i, 0].imshow(np.log10(image1), vmin = 4.5, vmax = 9.5)
+                im2 = axes[i, 1].imshow(np.log10(image2), vmin = 4.5, vmax = 9.5)
+
+
+                bar_len_kpc = 1.
+                bar_len_pix = 1.*N/W1[0].value * bar_len_kpc
+                y_bar_start_pix = 0.5*N
+                y_bar_end_pix = y_bar_start_pix + bar_len_pix
+                x_bar_pix = 0.9*N
+                axes[i, 0].plot([x_bar_pix, x_bar_pix], [y_bar_start_pix, y_bar_end_pix], color = 'white', linewidth = 3)
+                axes[i, 0].annotate("%i kpc"%bar_len_kpc, (x_bar_pix-0.08*N, y_bar_start_pix- 0.04*N), color = 'white', fontsize = 15, rotation = 0)
 
 
 
-            bar_len_kpc = 10
-            bar_len_pix = 1.*N/W2[0].value * bar_len_kpc
-            y_bar_start_pix = 0.5*N
-            y_bar_end_pix = y_bar_start_pix + bar_len_pix
-            x_bar_pix = 0.9*N
-            axes[i, 1].plot([x_bar_pix, x_bar_pix], [y_bar_start_pix, y_bar_end_pix], color = 'white', linewidth = 3)
-            axes[i, 1].annotate("%i kpc"%bar_len_kpc, (x_bar_pix-0.10*N, y_bar_start_pix- 0.04*N), color = 'white', fontsize = 15, rotation = 0)
-        for ax in axes.ravel():  ax.axis('off')
-        cax = fig.add_axes([0.90, 0.0, 0.02, 1.0])
-        cbr = fig.colorbar(im1, cax=cax,orientation="vertical")
-        cbr.set_label('projected gas density (M$_{\odot}$ kpc$^{-2}$)', fontsize = 15)
-        fig.subplots_adjust(left = 0.0, right = 0.92, top =1.0, bottom = 0.0, hspace = 0.0, wspace = 0.0)
-        figname = '%s_%i.png'%(snapname, sat_n)
-        fig.savefig('%s/%s'%(figdir, figname), dpi = 500)
+                bar_len_kpc = 10
+                bar_len_pix = 1.*N/W2[0].value * bar_len_kpc
+                y_bar_start_pix = 0.5*N
+                y_bar_end_pix = y_bar_start_pix + bar_len_pix
+                x_bar_pix = 0.9*N
+                axes[i, 1].plot([x_bar_pix, x_bar_pix], [y_bar_start_pix, y_bar_end_pix], color = 'white', linewidth = 3)
+                axes[i, 1].annotate("%i kpc"%bar_len_kpc, (x_bar_pix-0.10*N, y_bar_start_pix- 0.04*N), color = 'white', fontsize = 15, rotation = 0)
+            for ax in axes.ravel():  ax.axis('off')
+            cax = fig.add_axes([0.90, 0.0, 0.02, 1.0])
+            cbr = fig.colorbar(im1, cax=cax,orientation="vertical")
+            cbr.set_label('projected gas density (M$_{\odot}$ kpc$^{-2}$)', fontsize = 15)
+            fig.subplots_adjust(left = 0.0, right = 0.92, top =1.0, bottom = 0.0, hspace = 0.0, wspace = 0.0)
+            figname = '%s_%i.png'%(snapname, sat_n)
+            fig.savefig('%s/%s'%(figdir, figname), dpi = 500)
 
 
 
