abe05b68b8227e1e8249ea0777069ef220957bdd
RaymondSimons <rsimons@jhu.edu>
2019-05-08 15:52:47 -0400

    default testing message

--- track_satellites.py
+++ track_satellites.py
@@ -144,7 +144,7 @@ if __name__ == '__main__':
     delt_DD = int(args['delt_DD'])
     haloname = args['haloname']
 
-    Parallel(n_jobs = 1)(delayed(run_tracker)(simname = simname, haloname = haloname, DD = DD) for DD in np.arange(min_DD, max_DD, delt_DD))
+    Parallel(n_jobs = -1)(delayed(run_tracker)(simname = simname, haloname = haloname, DD = DD) for DD in np.arange(min_DD, max_DD, delt_DD))
     
 
 
