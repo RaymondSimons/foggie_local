ca14e536dc386bef70bc7c8f48e9bf34588e9cdc
RaymondSimons <rsimons@jhu.edu>
2019-05-10 01:36:31 -0400

    default testing message

--- make_satfig_qsubfiles.py
+++ make_satfig_qsubfiles.py
@@ -22,7 +22,7 @@ for sim_name in ['natural', 'nref11n_v2_selfshield_z15', 'nref11n_v3_selfshield_
         qf = open('/nobackupp2/rcsimons/foggie_momentum/submit_scripts/movie/%s'%qsub_fname, 'w+')
         qf.write('#PBS -S /bin/bash\n')
         qf.write('#PBS -l select=1:ncpus=16:model=san\n')
-        qf.write('#PBS -l walltime=3:00:00\n')
+        qf.write('#PBS -l walltime=4:00:00\n')
         qf.write('#PBS -q normal\n')
         qf.write('#PBS -N %s\n'%sim_snap_name)
         qf.write('#PBS -M rsimons@jhu.edu\n')
