4cbb747f0dd71b75ef579301c6eda59e884a49fb
RaymondSimons <rsimons@jhu.edu>
2019-05-12 17:24:28 -0400

    default testing message

--- interpolate_centers.py
+++ interpolate_centers.py
@@ -16,7 +16,7 @@ simnames = ['natural',
             'nref11n_nref10f',
             'nref11c_nref9f']
 
-simnames = ['nref11c_nref9f']
+#simnames = ['nref11c_nref9f']
 
 DD_arr = arange(90, 1010, 20)
 DD_plt = linspace(0, 1500, 1000)
@@ -24,17 +24,22 @@ for s, simname in enumerate(simnames):
     print (simname)
     fig, axes = plt.subplots(6,6, figsize = (24, 24))
     coord_datas_all = {}  
-    for st in arange(6):
+    for st in arange(7):
         xs, ys, zs, DDs  = [], [], [], []
         for d, DD in enumerate(DD_arr):
             fname = '/nobackupp2/rcsimons/foggie_momentum/anchor_files/halo_008508/anchor_props/%s/%s_DD%.4i_anchorprops.fits'%(simname, simname, DD)
             if os.path.isfile(fname):
                 data = fits.open(fname)
-                if len(data['SAT_%.2i'%st].data['box_avg']) > 0:
+                if st <  6: hd_name = 'SAT_%.2i'%st
+                if st == 6: hd_name = 'CENTRAL'
+                if len(data[hd_name].data['box_avg']) > 0:
                     DDs.append(DD)
-                    xs.append(data['SAT_%.2i'%st].data['box_avg'][0])
-                    ys.append(data['SAT_%.2i'%st].data['box_avg'][1])
-                    zs.append(data['SAT_%.2i'%st].data['box_avg'][2])
+                    xs.append(data[hd_name].data['box_avg'][0])
+                    ys.append(data[hd_name].data['box_avg'][1])
+                    zs.append(data[hd_name].data['box_avg'][2])
+
+
+
         axes[st, 0].plot(DDs, xs,'ro', markersize = 2, zorder = 1)
         axes[st, 2].plot(DDs, ys,'ro', markersize = 2, zorder = 1)
         axes[st, 4].plot(DDs, zs,'ro', markersize = 2, zorder = 1)
@@ -68,7 +73,8 @@ for s, simname in enumerate(simnames):
         dic_2['fze'] = fze
 
 
-        coord_datas_all['SAT_%.2i'%st] = dic_2
+        coord_datas_all[hd_name] = dic_2
+        
 
 
         for a, coord_data in enumerate(coord_datas):
@@ -83,11 +89,11 @@ for s, simname in enumerate(simnames):
 
         #axes[st,1].plot(DD_plt, fy(DD_plt), color = 'black', linestyle = 'dashed', alpha = 0.3,zorder = 3)
         #axes[st,2].plot(DD_plt, fz(DD_plt), color = 'black', linestyle = 'dashed', alpha = 0.3,zorder = 3)
+        if st < 6: ann_str = 'SAT %.2i'%st
+        if st == 6: ann_str = 'CENTRAL'
+        axes[st,0].annotate(ann_str, (0.1, 0.9), va = 'top', xycoords = 'axes fraction', fontsize = 20)
 
-        axes[st,0].annotate('SAT %.2i'%st, (0.1, 0.9), va = 'top', xycoords = 'axes fraction', fontsize = 20)
-
-        for ax in axes[st]: 
-            ax.set_xlabel('DD')
+        for ax in axes[st]: ax.set_xlabel('DD')
 
         axes[st,0].set_ylabel('x-box (kpc)')
         axes[st,1].set_ylabel('f(DD) - x-box (kpc)')
