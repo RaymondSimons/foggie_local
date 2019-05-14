1463de7b812f8ae755a4afeffaa17765ca74a5a7
RaymondSimons <rsimons@jhu.edu>
2019-05-13 23:28:32 -0400

    default testing message

--- combine_frames.py
+++ combine_frames.py
@@ -48,7 +48,7 @@ for sat in sats:
                 #for imgs in [imgs_12, imgs_34, imgs_56]:
                 min_shape = sorted([(np.sum(i.size), i.size ) for i in imgs])[0][1]
                 imgs_comb_temp = np.vstack((np.asarray( i.resize(min_shape) ) for i in imgs ))
-                imgs_comb_temp = Image.fromarray( imgs_comb)
+                imgs_comb_temp = Image.fromarray( imgs_comb_temp)
                 #imgs_comb.append(imgs_comb_temp)
                 imgs_comb_temp.save(fname_out)
 
