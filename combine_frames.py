b5eeb67cdcf2e8900c7a6ed2983b2810631ec1dc
RaymondSimons <rsimons@jhu.edu>
2019-05-13 23:04:08 -0400

    default testing message

--- combine_frames.py
+++ combine_frames.py
@@ -1,1 +1,16 @@
 import os
+
+
+
+DDs = arange(49, 1000)
+simnames =  ['natural',
+            'natural_v2',
+            'natural_v3',
+            'natural_v4',
+            'nref11c_nref9f',
+            'nref11n_nref10f']
+
+
+for d, DD in enumerate(DDs):
+    for s, simname in enumerate(simnames):
+        print (DD, simname)
