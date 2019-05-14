2ed4543c66c0a6098465c0298c98f981a54fb08a
RaymondSimons <rsimons@jhu.edu>
2019-05-14 00:12:41 -0400

    default testing message

--- combine_frames.py
+++ combine_frames.py
@@ -6,6 +6,30 @@ from joblib import Parallel, delayed
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+
+def parse():
+    '''
+    Parse command line arguments
+    ''' 
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     description='''\
+                                Generate the cameras to use in Sunrise and make projection plots
+                                of the data for some of these cameras. Then export the data within
+                                the fov to a FITS file in a format that Sunrise understands.
+                                ''')
+    parser.add_argument('-ax', '--ax', default=None, help='DD to use')
+    parser.add_argument('-sat', '--sat', default=None, help='DD to use')
+    parser.add_argument('-zoom', '--zoom', default=None, help='DD to use')
+
+    args = vars(parser.parse_args())
+    return args
+
+
+
+
+
+
+
 DDs = arange(49, 200)
 simnames =  ['natural',
             'natural_v2',
@@ -13,6 +37,8 @@ simnames =  ['natural',
             'natural_v4',
             'nref11c_nref9f',
             'nref11n_nref10f']
+
+'''
 axs = ['x', 'y', 'z']
 zooms = ['zoomin', 'zoomout', 'zoomoutfar']
 sats = arange(6)
@@ -29,16 +55,20 @@ simnames =  ['natural',
 axs = ['z']
 zooms = ['zoomin']
 sats = arange(1)
-
+'''
 
 def combine_frames(sat, ax, zoom, DD, simnames):
     print (DD)
     imgs = [] 
     fname_out = '/nobackupp2/rcsimons/foggie_momentum/sat_figures/combined/%s/%s/%.4i_%.2i_%s_%s.png'%(ax, zoom, DD, sat, ax, zoom)
+
     for s, simname in enumerate(simnames):
         fname = '/nobackupp2/rcsimons/foggie_momentum/sat_figures/%s/%s/%s/%s_%.4i_%.2i_%s_%s.png'%(simname, ax, zoom, simname, DD, sat, ax, zoom)
-        im =  Image.open(fname)
-        imgs.append(im)
+        if os.path.isfile(im):
+            im =  Image.open(fname)
+            imgs.append(im)
+        else:
+            return
     imgs_12 = imgs[0:2]
     imgs_34 = imgs[2:4]
     imgs_56 = imgs[4:6]
@@ -50,6 +80,17 @@ def combine_frames(sat, ax, zoom, DD, simnames):
     imgs_comb_temp = Image.fromarray( imgs_comb_temp)
     imgs_comb_temp.save(fname_out)
 
+
+if __name__ == '__main__':
+    args = parse()
+    ax = args['ax'] 
+    sat = args['sat'] 
+    zoom = args['zoom'] 
+    Parallel(n_jobs = -1)(delayed(combine_frames)(sat, ax, zoom, DD, simnames) for d, DD in enumerate(DDs))
+    png_names = '/nobackupp2/rcsimons/foggie_momentum/sat_figures/combined/%s/%s/'%(ax, zoom) + '%4d' + '_%.2i_%s_%s.png'%(sat, ax, zoom)
+    os.system('ffmpeg -r 24 -f image2 -s 1920x1080 -start_number 49 -i %s -vframes 1000 -vcodec libx264 -crf 25  -pix_fmt yuv420p /nobackupp2/rcsimons/foggie_momentum/sat_figures/movies/%.2i_%s_%s.mp4'%(png_names, sat, ax, zoom))
+
+'''
 #Combine Frames
 for sat in sats:
     for a, ax in enumerate(axs):
@@ -63,7 +104,7 @@ for sat in sats:
             png_names = '/nobackupp2/rcsimons/foggie_momentum/sat_figures/combined/%s/%s/'%(ax, zoom) + '%4d' + '_%.2i_%s_%s.png'%(sat, ax, zoom)
             os.system('ffmpeg -r 24 -f image2 -s 1920x1080 -start_number 49 -i %s -vframes 1000 -vcodec libx264 -crf 25  -pix_fmt yuv420p /nobackupp2/rcsimons/foggie_momentum/sat_figures/movies/%.2i_%s_%s.mp4'%(png_names, sat, ax, zoom))
                 
-
+'''
 
 
 
---
+++ make_movie_qsubfiles.py
@@ -0,0 +1,53 @@
+
+#sim_name = 'nref11n_nref10f_selfshield_z6'
+#sim_name = 'nref11n_selfshield_z15'
+import numpy
+from numpy import *
+
+#for sim_name in ['natural', 'nref11n_nref10f_selfshield_z6']:
+#for sim_name in ['natural', 'nref11n_nref10f_selfshield_z6']:
+
+
+
+
+axs = ['x', 'y', 'z']
+zooms = ['zoomin', 'zoomout', 'zoomoutfar']
+sats = arange(6)
+
+
+
+
+sf = open('/nobackupp2/rcsimons/foggie_momentum/submit_scripts/movies/submit_%s_%i_%i_movie_qsub.sh'%(sim_name, DDmin, DDmax), 'w+')
+for sat in sats:
+    for a, ax in enumerate(axs):
+        for z, zoom in enumerate(zooms):
+            snap_name = '%s_%.2i_%s'%(ax, sat, zoom)
+            sim_snap_name = snap_name + '_movie'
+
+            qsub_fname = '%s.qsub'%(sim_snap_name)
+
+            qf = open('/nobackupp2/rcsimons/foggie_momentum/submit_scripts/movies/%s'%qsub_fname, 'w+')
+            
+            qf.write('#PBS -S /bin/bash\n')
+            qf.write('#PBS -l select=1:ncpus=30:model=has\n')
+            qf.write('#PBS -l walltime=1:00:00\n')
+            qf.write('#PBS -q normal\n')
+            qf.write('#PBS -N %s\n'%sim_snap_name)
+            qf.write('#PBS -M rsimons@jhu.edu\n')
+            qf.write('#PBS -m abe\n')
+            qf.write('#PBS -o ./outfiles/%s_pbs.out\n'%sim_snap_name)
+            qf.write('#PBS -e ./outfiles/%s_pbs.err\n'%sim_snap_name)
+            qf.write('#PBS -V\n')
+            qf.write('#PBS -W group_list=s1938\n\n\n\n')  
+
+            for DDi in arange(DD, min(DD + N_split, DDmax)):
+                qf.write('/u/rcsimons/scripts/foggie_local/combine_frames.py --ax %i --sat  %s --zoom > ./outfiles/%s.err > ./outfiles/%s.out\n'%(ax, sat, zoom, sim_name, sim_snap_name, sim_snap_name))
+
+
+            qf.close()
+
+
+            sf.write('qsub %s\n'%qsub_fname)
+
+
+sf.close()
