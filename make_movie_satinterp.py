a3f25b621e19a0a75548de86aff40f8a72f535e6
RaymondSimons <rsimons@jhu.edu>
2019-05-28 16:56:14 -0400

    default testing message

--- make_movie_satinterp.py
+++ make_movie_satinterp.py
@@ -97,7 +97,7 @@ def make_figure(figdir, DD, cen_name, simdir, haloname, simname,  wd = 30, wdd =
         W3 = yt.YTArray([wd3, wd3, wd3], 'kpc')
 
 
-        for axis in ['x', 'y', 'z']:
+        for axis in ['x']:#@, 'y', 'z']:
             if not os.path.isfile('%s/%s/%s/%s_%.4i_%.2i_%s_zoomoutfar.png'%(figdir,axis, 'zoomoutfar',  cen_name, DD, 5, axis)):
                 if axis == 'x':                
                     box2 = ds.r[cen_central[0] - 0.5 * yt.YTArray(wdd2, 'kpc'):   cen_central[0] + 0.5 * yt.YTArray(wdd2, 'kpc'), \
