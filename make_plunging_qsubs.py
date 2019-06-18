90a4b199b43ab8ce36a5dbef7c4cff407a53f237
RaymondSimons <rsimons@jhu.edu>
2019-06-17 22:59:15 -0400

    default testing message

--- make_plunging_qsubs.py
+++ make_plunging_qsubs.py
@@ -22,7 +22,7 @@ for simname in ['natural', 'natural_v2', 'natural_v3', 'natural_v4', 'nref11n_nr
         qf.write('#PBS -W group_list=s1698\n\n\n\n')  
 
         qf.write('python /u/rcsimons/scripts/foggie_local/plunging_orbits.py \
-             -simname %s  --DD %i> ./out/%s.err > ./out/%s.out\n'%(simname, DD, snapname, snapname))
+             -simname %s  -DD %i > ./out/%s.err > ./out/%s.out\n'%(simname, DD, snapname, snapname))
 
         qf.close()
 
