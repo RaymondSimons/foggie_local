48194d8bed8b6f10f38fba928cc183300d938507
RaymondSimons <rsimons@jhu.edu>
2019-06-17 22:28:01 -0400

    default testing message

---
+++ make_plunging_qsubs.py
@@ -0,0 +1,28 @@
+import numpy as np
+from numpy import *
+
+for simname in ['natural', 'natural_v2', 'natural_v3', 'natural_v4', 'nref11n_nref10f', 'nref11c_nref9f']:
+    snapname = '%s_particles'%(simname)
+    qsub_fname = '%s_particles.qsub'%(simname)
+
+    qf = open('/nobackupp2/rcsimons/foggie_momentum/submit_scripts/plunges/%s'%qsub_fname, 'w+')
+
+    qf.write('#PBS -S /bin/bash\n')
+    qf.write('#PBS -l select=1:ncpus=12:model=san\n')
+    qf.write('#PBS -l walltime=1:00:00\n')
+    qf.write('#PBS -q normal\n')
+    qf.write('#PBS -N %s\n'%snapname)
+    qf.write('#PBS -M rsimons@jhu.edu\n')
+    qf.write('#PBS -m abe\n')
+    qf.write('#PBS -o ./out/%s_pbs.out\n'%snapname)
+    qf.write('#PBS -e ./out/%s_pbs.err\n'%snapname)
+    qf.write('#PBS -V\n')
+    qf.write('#PBS -W group_list=s1698\n\n\n\n')  
+
+    qf.write('/u/rcsimons/scripts/foggie_local/write_particles.py \
+         -simname %s  > ./out/%s.err > ./out/%s.out\n'%(simname, snapname, snapname))
+
+    qf.close()
+
+
+    sf.write('qsub %s\n'%qsub_fname)
