45d13049b82de7f2571f97e347618734b08088c2
RaymondSimons <rsimons@jhu.edu>
2019-08-03 16:45:57 -0400

    default testing message

--- RP_sweeping.py
+++ RP_sweeping.py
@@ -6,13 +6,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import *
 from numpy import *
+from consistency import *
+
 plt.ioff()
 plt.close('all')
 
 ds = yt.load('/user/rsimons/foggie/sims/halo_008508/nref11n_nref10f/DD0809/DD0809')
 
 
-wdd = 10
+
 wd = 30
 
 x_w = wd
@@ -24,7 +26,7 @@ W = YTArray([x_w, y_w, z_w], 'kpc')
 cen_g = YTArray([31597.918859155205, 31096.55501686099, 32192.674736309542], 'kpc')
 
 
-for i in arange(300, 320):
+for i in arange(300, 301):
     print (i)
     L = [1*cos(pi*(i)/100.),0, 1*sin(pi*(i)/100.)] # vector normal to cutting plane
 
@@ -33,16 +35,16 @@ for i in arange(300, 320):
     fig, ax = plt.subplots(1,1, figsize = (10, 10))
     ax.axis('off')
 
-    box = ds.r[cen_g[0] - 0.5 * yt.YTArray(max([wdd, wd]), 'kpc'): cen_g[0] + 0.5 * yt.YTArray(max([wdd, wd]), 'kpc'), \
-               cen_g[1] - 0.5 * yt.YTArray(min([3, wd]), 'kpc'): cen_g[1] + 0.5 * yt.YTArray(max([3, wd]), 'kpc'), \
-               cen_g[2] - 0.5 * yt.YTArray(max([wdd, wd]), 'kpc'): cen_g[2] + 0.5 * yt.YTArray(max([wdd, wd]), 'kpc')]
+    box = ds.r[cen_g[0] - yt.YTArray(wd/2.,  'kpc'): cen_g[0] + 0.5 * yt.YTArray(wd/2., 'kpc'), \
+               cen_g[1] - yt.YTArray(wd/2., 'kpc'): cen_g[1] + 0.5 * yt.YTArray( wd/2., 'kpc'), \
+               cen_g[2] - yt.YTArray(wd/2., 'kpc'): cen_g[2] + 0.5 * yt.YTArray( wd/2., 'kpc')]
 
 
     image1 = yt.off_axis_projection(box, cen_g, L, W, N, ('gas', 'density'), north_vector =  north_vector)
 
 
     image1 = image1.in_units('Msun * kpc**-2')
-    ax.imshow(np.log10(image1), vmin = 4.5, vmax = 9.5)    
+    ax.imshow(np.log10(image1), vmin = density_proj_min, vmax = density_proj_max, cmap = density_color_map)    
     fig.subplots_adjust(left = 0.0, right = 0.92, top =1.0, bottom = 0.0, hspace = 0.0, wspace = 0.0)
     fig.savefig("/user/rsimons/foggie/figures/movies/ram_pressure/frame_%i.png"%i, dpi = 300)
 
