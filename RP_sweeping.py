a69aef90ffaaa097ca0d22b99c03f19777cb86fe
RaymondSimons <rsimons@jhu.edu>
2019-08-03 17:13:19 -0400

    default testing message

--- RP_sweeping.py
+++ RP_sweeping.py
@@ -15,7 +15,7 @@ ds = yt.load('/user/rsimons/foggie/sims/halo_008508/nref11n_nref10f/DD0809/DD080
 
 
 
-wd = 10.
+wd = 30.
 
 x_w = wd
 y_w = wd
@@ -28,6 +28,11 @@ cen_g = YTArray([31597.918859155205, 31096.55501686099, 32192.674736309542], 'kp
 
 for i in arange(300, 301):
     print (i)
+    box = ds.r[cen_g[0] - yt.YTArray(wd,  'kpc'): cen_g[0] + yt.YTArray( wd, 'kpc'), \
+               cen_g[1] - yt.YTArray(wd, 'kpc'):  cen_g[1] +  yt.YTArray(wd, 'kpc'), \
+               cen_g[2] - yt.YTArray(wd, 'kpc'):  cen_g[2] +  yt.YTArray(wd, 'kpc')]
+
+    '''
     L = [1*cos(pi*(i)/100.),0, 1*sin(pi*(i)/100.)] # vector normal to cutting plane
 
     N = 512
@@ -35,19 +40,25 @@ for i in arange(300, 301):
     fig, ax = plt.subplots(1,1, figsize = (10, 10))
     ax.axis('off')
 
-    box = ds.r[cen_g[0] - yt.YTArray(wd,  'kpc'): cen_g[0] + yt.YTArray( wd, 'kpc'), \
-               cen_g[1] - yt.YTArray(wd, 'kpc'):  cen_g[1] +  yt.YTArray(wd, 'kpc'), \
-               cen_g[2] - yt.YTArray(wd, 'kpc'):  cen_g[2] +  yt.YTArray(wd, 'kpc')]
-
 
     image1 = yt.off_axis_projection(box, cen_g, L, W, N, ('gas', 'density'), north_vector =  north_vector)
-
-
+    '''
+    axis = 'x'
+    p = yt.ProjectionPlot(ds, axis, ("gas","density"), center = cen_g, data_source=box, width=W)
+    p.set_unit(('gas','density'), 'Msun/pc**2')
+    p.set_zlim(('gas', 'density'), zmin = density_proj_min, zmax =  density_proj_max)
+    p.set_cmap(('gas', 'density'), density_color_map)
+    p.annotate_timestamp(corner='upper_left', redshift=True, draw_inset_box=True)
+    p.hide_axes()
+    p.annotate_timestamp(corner='upper_left', redshift=True, draw_inset_box=True)
+    p.annotate_scale(size_bar_args={'color':'white'})
+    p.save("/user/rsimons/foggie/figures/movies/ram_pressure/frame_%i.png"%i)
+    '''
     image1 = image1.in_units('Msun/pc**2')
     ax.imshow(np.log10(image1), cmap = density_color_map)    
     fig.subplots_adjust(left = 0.0, right = 0.92, top =1.0, bottom = 0.0, hspace = 0.0, wspace = 0.0)
     fig.savefig("/user/rsimons/foggie/figures/movies/ram_pressure/frame_%i.png"%i, dpi = 300)
-
+    '''
 
 
 
