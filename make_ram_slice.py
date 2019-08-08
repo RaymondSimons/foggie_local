2647a038f89e5633a9e98def1bc7c5cf90bf56f0
RaymondSimons <rsimons@jhu.edu>
2019-08-07 22:48:21 -0400

    default testing message

---
+++ make_ram_slice.py
@@ -0,0 +1,112 @@
+import yt
+from yt.units import kpc, Mpc
+import joblib
+from joblib import Parallel, delayed
+import os
+from astropy.io import ascii
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import *
+from astropy import constants as c
+from astropy.cosmology import Planck13 as cosmo
+import scipy
+from scipy import stats
+from yt.fields.api import ValidateParameter
+
+def ram_slice(haloname):
+    center_dic =  np.load('/Users/rsimons/Desktop/foggie/ouputs/centers/%s_centers.npy'%haloname, allow_pickle = True)[()]
+    to_save = {}
+
+    if haloname == 'halo_008508': 
+        simnames =  ['natural', 'nref11c_nref9f', 'nref11n_nref10f'] 
+        DDname = 'DD0487'
+    else: 
+        simnames = ['natural', 'nref11c_nref9f']
+        DDname = 'DD0581'
+
+    for simname in simnames:
+        to_save_sim = {}
+        print ('new', haloname, simname)
+
+
+                
+        def _rampressure(field, data):
+
+
+            cen_bulkv = data.get_field_parameter("bulk_velocity").in_units('km/s')
+
+            velx = data['enzo', 'x-velocity'].to('km/s') - cen_bulkv[0]
+            vely = data['enzo', 'y-velocity'].to('km/s') - cen_bulkv[1]
+            velz = data['enzo', 'z-velocity'].to('km/s') - cen_bulkv[2]
+
+            vel_mean = np.sqrt(velx**2. + vely**2. + velz**2.)/np.sqrt(3)
+
+            return data['density'] * vel_mean**2.
+
+
+
+
+        yt.add_field(("gas", "ram_pressure"), function=_rampressure, units="kg * km/s**2/kpc**2", validators=[ValidateParameter(['center', 'bulk_velocity'])])
+        center = center_dic['center_%s'%simname].to('kpc')
+
+        flname = '/Users/rsimons/Desktop/foggie/sims/%s/%s/%s/%s'%(haloname, simname, DDname, DDname)
+
+
+
+        ds = yt.load(flname)
+        sp = ds.sphere(center, (200, 'kpc'))
+
+        N = 10
+        if not os.path.exists('/Users/rsimons/Dropbox/file_transfer/%s'%haloname):
+            os.system('mkdir /Users/rsimons/Dropbox/file_transfer/%s'%haloname)
+        if not os.path.exists('/Users/rsimons/Dropbox/file_transfer/%s/%s'%(haloname, simname)):
+            os.system('mkdir /Users/rsimons/Dropbox/file_transfer/%s/%s'%(haloname, simname))
+
+        for i in arange(N):
+            L = [max(1*cos(pi*(i)/100.), 1.e-5),0, max(1*sin(pi*(i)/100.), 1.e-5)] # vector normal to cutting plane
+            north_vector = [-1,1,0]
+            W = yt.YTArray([100, 100, 100], 'kpc')
+            num_pix = 512
+            #image1 = yt.off_axis_projection(sp, center, L, W, N, ('gas', 'ram_pressure'), north_vector =  north_vector, weight = ('gas', 'cell_mass'))
+            #image1 = image1.in_units('kg * km/s**2/kpc**2')
+            #fig, axes = plt.subplots(1,1, figsize = (5, 5))
+            #im1 = axes.imshow(np.log10(image1), vmin = 18, vmax = 29)
+            #fig.savefig('/Users/rsimons/Dropbox/file_transfer/%s/%s/projection_RP_%s_%s_%s.png'%(haloname, simname,haloname, simname, i))
+
+
+
+            slc = yt.OffAxisProjectionPlot(ds,  L, 'ram_pressure', center = center,width = (100, 'kpc'), weight_field = ('gas', 'cell_mass'), north_vector = north_vector, data_source = sp)
+            #slc = yt.OffAxisSlicePlot(ds,  L, 'ram_pressure', center = center, width = (100, 'kpc'))#, north_vector = north_vector)
+            slc.set_zlim(1.e20, 1.e26)
+            print ('Saving slice...')
+            slc.save('/Users/rsimons/Dropbox/file_transfer/%s/%s/slice_RP_%s_%s_%s.png'%(haloname, simname,haloname, simname, i))
+
+            '''
+            #slc = yt.OffAxisProjectionPlot(ds, L, 'density', center = center, width = (100, 'kpc'))#, north_vector = north_vector)
+            slc = yt.OffAxisSlicePlot(ds, L, 'density', center = center, width = (100, 'kpc'))#, north_vector = north_vector)
+
+            print ('Saving slice...')
+            slc.save('/Users/rsimons/Dropbox/file_transfer/%s/%s/slice_density_%s_%s_%i.png'%(haloname, simname,haloname, simname, i))
+            '''
+
+
+if __name__ == '__main__':
+
+    halonames = np.array(['halo_002878',
+                 'halo_004123',
+                 'halo_005016',
+                 'halo_005036',
+                 'halo_002392',
+                 'halo_008508'])
+    halonames = np.array([
+                 'halo_008508'])
+
+
+
+
+
+
+    for haloname in halonames: ram_slice(haloname)
+
+
+
