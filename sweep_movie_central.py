f69bff28107e69cbeac78b7b5c9c3d3cabe4a483
RaymondSimons <rsimons@jhu.edu>
2020-03-26 12:17:29 -0400

    default testing message

--- sweep_movie_central.py
+++ sweep_movie_central.py
@@ -189,194 +189,57 @@ def make_off_axis_projection_plots(ds, center, box_proj, fig_dir, haloname, norm
     return prj
 
 
+def load_sim(args):
+    foggie_dir, output_dir, run_loc, trackname, haloname, spectra_dir, infofile = get_run_loc_etc(args)
+    track_dir =  trackname.split('halo_tracks')[0]   + 'halo_infos/00' + args.halo + '/' + args.run + '/'
+    snap_name = foggie_dir + run_loc + args.output + '/' + args.output
+    ds, refine_box, refine_box_center, refine_width = load(snap = snap_name, 
+                                                           trackfile = trackname, 
+                                                           use_halo_c_v=args.use_halo_c_v, 
+                                                           halo_c_v_name=track_dir + 'halo_c_v')
+    return ds, refine_box, refine_width
+
 
 if __name__ == '__main__':
 
     args = parse_args()
-    print (args.system)
-    #Run this in series on all of the halos
-    if args.run_all:
-        inputs = [('2878', 'DD0581'), 
-                  ('5016', 'DD0581'), 
-                  ('5036', 'DD0581'),
-                  ('2392', 'DD0581'),
-                  ('4123', 'DD0581'),
-                  ('8508', 'DD0487')]
-        inputs = inputs
-    else:
-        inputs = [(args.halo, args.output),]
+    inputs = [(args.halo, args.output),]
 
-    if args.system == 'jase':
-        sat_cat = ascii.read('/Users/rsimons/Dropbox/foggie/catalogs/satellite_properties.cat')        
-        fig_dir = '/Users/rsimons/Dropbox/foggie/figures/off_axis_satellite_projections'
-    else:
-        sat_cat = ascii.read('/nobackupp2/rcsimons/foggie/catalogs/satellite_properties.cat')        
-        fig_dir = '/nobackupp2/rcsimons/foggie/figures/off_axis_satellite_projections'
+    fig_dir = '/nobackupp2/rcsimons/foggie/figures/off_axis_satellite_projections'
 
-    for args.halo, args.output in inputs:
-        sat_cat_halo = sat_cat[sat_cat['halo'] == int(args.halo)]
+    ds, refine_box, refine_width = load_sim(args)
 
+    sat_center = ds.halo_center_kpc
+    box_proj = refine_box
+    box_width = ds.arr(100, 'code_length').to('kpc')
 
-        foggie_dir, output_dir, run_loc, trackname, haloname, spectra_dir, infofile = get_run_loc_etc(args)
-        run_dir = foggie_dir + run_loc
+    min_nrot = 0
+    max_nrot = 200
 
-        ds_loc = run_dir + args.output + "/" + args.output
-        ds = yt.load(ds_loc)
-        track = Table.read(trackname, format='ascii')
-        track.sort('col1')
-        zsnap = ds.get_parameter('CosmologyCurrentRedshift')
+    print ('pre-loading grid data')
+    grid_fields = [('deposit', 'stars_density'), ('gas', 'density'),\
+                   ('deposit', 'dm_density'), ('gas', 'temperature'),\
+                   ('gas', 'metallicity')]
+    for (n1, n2) in grid_fields:
+        print ('\t loading (%s, %s)'%(n1, n2))
+        temp_variable = box_proj[n1, n2]
 
-        refine_box, refine_box_center, x_width = get_refine_box(ds, zsnap, track)
-        filter_particles(refine_box)
+    start_arrows = []
+    end_arrows = []
 
+    if not os.path.exists(fig_dir + '/' + args.halo + '/' + args.output):
+        os.system('mkdir ' + fig_dir + '/' + args.halo + '/' + args.output)
 
-        small_sp = ds.sphere(refine_box_center, (20, 'kpc'))
-        refine_box_bulk_vel = small_sp.quantities.bulk_velocity()
+    for nrot in np.arange(min_nrot, max_nrot):
+        fname = fig_dir + '/combined/%s_%s_%.3i.png'%(haloname, sat['id'], nrot)
 
-        #def run_sat(ds, sat, refine_box, refine_box_center, args): 
-        for sat in sat_cat_halo:
-            #movie_fname = fig_dir + '/movies/%s_%s.mp4'%(haloname, sat['id'])
-            #if os.path.exists(movie_fname): continue
-            if args.do_central:
-                if sat['id'] != '0': continue
-                sat_center = ds.arr(refine_box_center, 'code_length').to('kpc') 
-                box_proj = refine_box
-                box_width =float( ds.arr(x_width, 'code_length').to('kpc').value)*kpc  
+        normal_vector = [0, np.cos(2*pi * (1.*nrot)/max_nrot), np.sin(2*pi * (1.*nrot)/max_nrot)]
 
-            else:
-                if sat['id'] == '0': continue
-                box_width = 10 * kpc
-                sat_center = ds.arr([sat['x'], sat['y'], sat['z']], 'kpc')     
-                #ensures that a 45 degree tilted box stays in the frame
-                box_proj = ds.r[sat_center[0]     - box_width/np.sqrt(2): sat_center[0] + box_width/np.sqrt(2),
-                                sat_center[1]     - box_width/np.sqrt(2): sat_center[1] + box_width/np.sqrt(2),
-                                sat_center[2]     - box_width/np.sqrt(2): sat_center[2] + box_width/np.sqrt(2),
-                            ]
-
-
-            if os.path.isdir(fig_dir + '/movies/%s_%s'%(args.halo, sat['id'])):
-                os.system('mkdir ' + fig_dir + '/movies/%s_%s'%(args.halo, sat['id']))
-
-
-            if not args.do_central:
-                sp = ds.sphere(center = ds.arr(sat_center, 'kpc'), radius = 1*kpc)
-
-                #sp.set_field_parameter('bulk_velocity', refine_box_bulk_vel)
-
-                stars_vx = sp.quantities.weighted_average_quantity(('stars', 'particle_velocity_x'), ('stars', 'particle_mass')).to('km/s') -  refine_box_bulk_vel[0]
-                stars_vy = sp.quantities.weighted_average_quantity(('stars', 'particle_velocity_y'), ('stars', 'particle_mass')).to('km/s') -  refine_box_bulk_vel[1]
-                stars_vz = sp.quantities.weighted_average_quantity(('stars', 'particle_velocity_z'), ('stars', 'particle_mass')).to('km/s') -  refine_box_bulk_vel[2]
-                
-                print (stars_vx)
-
-
-                np.random.seed(1)
-
-                sat_velocity = ds.arr([stars_vx, stars_vy, stars_vz])
-                sat_velocity_norm = sat_velocity/np.linalg.norm(sat_velocity)
-                start_arrow = sat_center
-                end_arrow = sat_center + sat_velocity_norm.value *  kpc
-                main_gal = sat_cat_halo[sat_cat_halo['id'] == '0']
-
-                to_center_vec = ds.arr([float(main_gal['x']), float(main_gal['y']), float(main_gal['z'])], 'kpc') -   sat_center  
-                end_arrow_2 = sat_center  + to_center_vec.value/np.linalg.norm(to_center_vec.value) * 1. * kpc
-
-
-
-                fname = fig_dir + '/combined/%s_%s_noarrow.png'%(haloname, sat['id'])
-
-                x = np.random.randn(3)
-                x -= x.dot(sat_velocity_norm.value) * sat_velocity_norm.value
-                normal_vector_1 = x/np.linalg.norm(x) 
-
-
-                norm_vector = normal_vector_1
-                prj = make_off_axis_projection_plots(ds, sat_center, box_proj, fig_dir + '/' + args.halo, haloname,norm_vector, north_vector = sat_velocity_norm.value, \
-                                    fig_end = '%s_noarrow'%(sat['id']), \
-                                    do = ['stars', 'gas', 'dm', 'temp', 'metal'], axes = ['off_axis'],  annotate_center = False,
-                                    add_velocity = False, add_arrow = False, start_arrow = [start_arrow], end_arrow = [end_arrow], proj_width = box_width)  
-
-                fl_dm = fig_dir + '/%s/%s_%s_dm_%s_noarrow.png'%(args.halo, haloname, 'off_axis', sat['id'])
-                fl_stars = fig_dir + '/%s/%s_%s_stars_%s_noarrow.png'%(args.halo, haloname, 'off_axis', sat['id'])
-                fl_gas = fig_dir + '/%s/%s_%s_gas_%s_noarrow.png'%(args.halo, haloname, 'off_axis', sat['id'])
-                fl_temp = fig_dir + '/%s/%s_%s_temp_%s_noarrow.png'%(args.halo, haloname, 'off_axis', sat['id'])
-                fl_metal = fig_dir + '/%s/%s_%s_metal_%s_noarrow.png'%(args.halo, haloname, 'off_axis', sat['id'])
-
-
-                fls = [fl_dm,  fl_stars, fl_gas, fl_temp, fl_metal]
-                fls = [fl_stars, fl_gas, fl_temp, fl_metal]
-
-                imgs = [PIL.Image.open(fl) for fl in fls]
-
-
-                min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
-                imgs_comb = np.hstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
-
-
-                imgs_comb = PIL.Image.fromarray( imgs_comb)
-                imgs_comb.save(fname)
-
-            if False:
-                if np.isnan(args.rot_n):
-                    min_nrot = 0
-                    max_nrot = 200
-                else:
-                    min_nrot = args.rot_n
-                    max_nrot = args.rot_n+1
-
-
-                if args.do_central:
-                        #preload these, i assume it makes it faster (instead of having to do it over and over again inside the function)
-                        print ('pre-loading grid data')
-                        grid_fields = [('deposit', 'stars_density'), ('gas', 'density'),\
-                                       ('deposit', 'dm_density'), ('gas', 'temperature'),\
-                                       ('gas', 'metallicity')]
-                        for (n1, n2) in grid_fields:
-                            print ('\t loading (%s, %s)'%(n1, n2))
-                            temp_variable = box_proj[n1, n2]
-
-                        start_arrows = []
-                        end_arrows = []
-                else:
-                    start_arrows = [start_arrow, start_arrow]
-                    end_arrows   = [end_arrow, end_arrow_2]
-
-                for nrot in np.arange(min_nrot, max_nrot):
-                    fname = fig_dir + '/combined/%s_%s_%.3i.png'%(haloname, sat['id'], nrot)
-
-                    normal_vector = [0, np.cos(2*pi * (1.*nrot)/max_nrot), np.sin(2*pi * (1.*nrot)/max_nrot)]
-
-                    prj = make_off_axis_projection_plots(ds = ds, center = sat_center, box_proj = box_proj, fig_dir = fig_dir + '/' + args.halo,
-                                                        haloname = haloname, normal_vector = normal_vector, north_vector = [1,0,0], \
-                                                        fig_end = '%s_%.3i'%(sat['id'], nrot), \
-                                                        do = ['stars', 'gas',  'dm', 'temp', 'metal'], axes = ['off_axis'], annotate_center = not args.do_central,
-                                                        add_velocity = False, add_arrow = not args.do_central, start_arrow = start_arrows, end_arrow = end_arrows, proj_width = box_width)  
-
-                    fl_dm = fig_dir + '/%s/%s_%s_dm_%s_%.3i.png'%(args.halo, haloname, 'off_axis', sat['id'], nrot)
-                    fl_stars = fig_dir + '/%s/%s_%s_stars_%s_%.3i.png'%(args.halo, haloname, 'off_axis', sat['id'], nrot)
-                    fl_gas = fig_dir + '/%s/%s_%s_gas_%s_%.3i.png'%(args.halo, haloname, 'off_axis', sat['id'], nrot)
-                    fl_temp = fig_dir + '/%s/%s_%s_temp_%s_%.3i.png'%(args.halo, haloname, 'off_axis', sat['id'], nrot)
-                    fl_metal = fig_dir + '/%s/%s_%s_metal_%s_%.3i.png'%(args.halo, haloname, 'off_axis', sat['id'], nrot)
-
-
-
-                    fls = [fl_dm,  fl_stars, fl_gas, fl_temp, fl_metal]
-                    imgs = [PIL.Image.open(fl) for fl in fls]
-
-
-                    min_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
-                    imgs_comb = np.hstack( (np.asarray( i.resize(min_shape) ) for i in imgs ) )
-
-
-                    imgs_comb = PIL.Image.fromarray( imgs_comb)
-                    imgs_comb.save(fname)
-
-
-                if ('jase' in args.system):
-                    os.system('ffmpeg -y -r 24 -f image2 -start_number 0 -i ' + fig_dir + '/combined/%s_%s_'%(haloname, sat['id']) + r'%03d.png ' +'-vframes 1000 -vcodec libx264 -crf 25  -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -pix_fmt yuv420p ' + fig_dir + '/movies/%s_%s.mp4'%(haloname, sat['id']))
-
-
-
+        prj = make_off_axis_projection_plots(ds = ds, center = sat_center, box_proj = box_proj, fig_dir = fig_dir + '/' + args.halo + '/' + args.output,
+                                            haloname = args.halo, normal_vector = normal_vector, north_vector = [1,0,0], \
+                                            fig_end = 'central_%.3i'%(nrot), \
+                                            do = ['stars', 'gas',  'dm', 'temp', 'metal'], axes = ['off_axis'], annotate_center = not args.do_central,
+                                            add_velocity = False, add_arrow = not args.do_central, start_arrow = start_arrows, end_arrow = end_arrows, proj_width = box_width)  
 
 
 
